29e7d08b5368dd3c09ae2494c6183c06dd2193a4
Sylvain Boudreau <sylvainboudreau@Sylvains-MacBook-Pro.local>
2019-09-29 20:27:49 -0300

    Meme Schema - image changed to image_url, date deleted, we have access to the date with imgur api

--- server/meme/meme.model.js
+++ server/meme/meme.model.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Meme = new Schema({
-  image: {
+  image_url: {
     type: String
-  },
-  date_added: {
-    type: Date
   },
   user_id: {
     type: String
